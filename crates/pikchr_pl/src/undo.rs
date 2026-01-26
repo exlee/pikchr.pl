f2f55077cec55c539000063034df13d95536eb7b
Przemysław Alexander Kamiński <exlee@users.noreply.github.com>
2026-01-26 12:49:48 +0100

    Make undo remember cursor position

--- crates/pikchr_pl/src/undo.rs
+++ crates/pikchr_pl/src/undo.rs
@@ -11,55 +11,81 @@
 // You should have received a copy of the GNU General Public License along
 // with pikchr.pl. If not, see <https://www.gnu.org/licenses/>.
 
-use iced::widget::text_editor;
+use iced::widget::text_editor::{self, Cursor, Position};
 
-#[derive(Default)]
+#[derive(Debug, Clone)]
+struct UndoContent {
+    content: text_editor::Content,
+    position: Position,
+}
+#[derive(Default, Debug)]
 pub struct UndoStack {
-    pub redo: Vec<text_editor::Content>,
-    pub undo: Vec<text_editor::Content>,
+    pub redo: Vec<UndoContent>,
+    pub undo: Vec<UndoContent>,
+}
+
+impl From<text_editor::Content> for UndoContent {
+    fn from(value: text_editor::Content) -> Self {
+
+        Self {
+            position: value.cursor().position,
+            content: value
+        }
+    }
+}
+
+impl From<&text_editor::Content> for UndoContent {
+    fn from(value: &text_editor::Content) -> Self {
+
+        Self {
+            position: value.cursor().position,
+            content: value.clone()
+        }
+    }
 }
 
 impl UndoStack {
     pub fn new(initial_content: text_editor::Content) -> Self {
         Self {
-            undo: vec![initial_content],
+            undo: vec![initial_content.into()],
             redo: vec![],
         }
     }
     pub fn push(&mut self, content: &text_editor::Content) {
         self.redo.clear();
-        self.undo.push(content.clone())
+        self.undo.push(content.into())
     }
     pub fn undo_into(&mut self, content: &mut text_editor::Content) {
-        if self.undo.is_empty() {
+        if self.undo.len()  <= 1 {
+            *content = self.undo.last().unwrap().content.clone();
             return;
         }
         if let Some(undo_layer) = self.undo.pop() {
             self.redo.push(undo_layer);
         }
 
-        let previous_state: text_editor::Content = self
+        let previous_state: &UndoContent = self
             .undo
             .last()
-            .expect("Guaranteed by length check above")
-            .to_owned();
+            .expect("Guaranteed by length check above");
 
-        *content = previous_state;
+        *content = previous_state.content.clone();
+        let new_cursor = Cursor {
+            position: previous_state.position,
+            selection: None
+        };
+        content.move_to(new_cursor);
     }
     pub fn redo_into(&mut self, content: &mut text_editor::Content) {
-        if self.undo.len() < 2 {
-            return;
-        }
         if let Some(redo_layer) = self.redo.pop() {
+
+            *content = redo_layer.content.clone();
+            let new_cursor = Cursor {
+                position: redo_layer.position,
+                selection: None
+            };
             self.undo.push(redo_layer);
-        }
-
-        let previous_state: text_editor::Content = self
-            .undo
-            .last()
-            .expect("Guaranteed by length check above")
-            .to_owned();
-
-        *content = previous_state;
+            content.move_to(new_cursor);
+        } 
     }
 }
